8c93403920ee944cce47e1fb3651516e1b75a333
ferbass <ferbass@gmail.com>
2024-03-27 14:25:54 +0900

    basic string and math operation

--- Ya.rb
+++ Ya.rb
@@ -7,14 +7,40 @@
 
 class Ya
   def eval(exp)
+
     if is_number(exp)
       return exp
+    end
+
+    if exp[0] == '+'
+      return eval(exp[1]) + eval(exp[2])
+    end
+
+    if exp[0] == '-'
+      return eval(exp[1]) - eval(exp[2])
+    end
+
+    if exp[0] == '*'
+      return eval(exp[1]) * eval(exp[2])
+    end
+
+    if exp[0] == '/'
+      return eval(exp[1]) / eval(exp[2])
+    end
+
+    if is_string(exp)
+      return exp.slice(1, exp.length - 2)
     end
 
     throw 'YaError: Invalid expression'
   end
 
-  def is_number(ext)
-    return ext.is_a? Numeric
+  def is_number(exp)
+    return exp.is_a? Numeric
   end
+
+  def is_string(exp)
+    return exp.is_a?(String) && exp.start_with?('"') && exp.end_with?('"')
+  end
+
 end
