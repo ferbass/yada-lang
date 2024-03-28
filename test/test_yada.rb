7cddfb902c6c58c4af2565fbba34769dba0d7897
ferbass <ferbass@gmail.com>
2024-03-28 10:25:16 +0900

    rename test_eval to test_self_eval

--- test/test_yada.rb
+++ test/test_yada.rb
@@ -3,6 +3,6 @@ require './yada.rb'
 require './environment'
 require './test/test_math'
 require './test/test_variables.rb'
-require './test/test_eval.rb'
+require './test/test_self_eval.rb'
 require './test/test_block.rb'
 
