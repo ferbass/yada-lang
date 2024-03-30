87ef187f0fe095e0a0db258a5fcd7b0d379f927d
ferbass <ferbass@gmail.com>
2024-03-30 20:51:56 +0900

    add ^ to the grammar

--- parser/yada_parser.rb
+++ parser/yada_parser.rb
@@ -316,7 +316,7 @@ class SyntaxToolTokenizer__
 [/\A\s+/, '_lex_rule3'],
 [/\A"[^\"]*"/, '_lex_rule4'],
 [/\A\d+/, '_lex_rule5'],
-[/\A[\w\-+*=<>!,\/]+/, '_lex_rule6'],
+[/\A[\w\-+*=<>!,^\/]+/, '_lex_rule6'],
 [/\A\s+/, '_lex_rule7']]
   @@lex_rules_by_conditions = {'INITIAL' => [0, 1, 2, 3, 4, 5, 6]}
 
