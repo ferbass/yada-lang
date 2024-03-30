bd59bff86d68bec714b9a4f72fbada7cde6b4027
ferbass <ferbass@gmail.com>
2024-03-30 20:10:32 +0900

    add more custom asserts to use Yada parser

--- test/test_util_yada.rb
+++ test/test_util_yada.rb
@@ -1,10 +1,47 @@
 require 'test/unit'
+require 'stringio'
 
 class TestUtil < Test::Unit::TestCase
+
+  module TestUtil
+  class AssertionMessage < StandardError; end
+  end
 
   def yada_assert_equal(yada, code, expected)
     exp = YadaParser.parse(code)
     assert_equal(yada.eval(exp), expected)
+  end
+
+  def yada_assert_not_equal(yada, code, expected)
+    exp = YadaParser.parse(code)
+    assert_not_equal(yada.eval(exp), expected)
+  end
+
+  def yada_assert_raises(yada, code, expected)
+    exp = YadaParser.parse(code)
+
+    error = nil
+    begin
+      capture_stdout { yada.eval(exp) }
+    rescue => e
+      error = e
+    end
+
+    unless error && error.message.include?(expected)
+      raise TestUtil::AssertionMessage, "Expected to raise an error containing '#{expected}', but got '#{error&.message}'"
+    end
+  end
+
+
+  private
+
+  def capture_stdout
+    original_stdout = $stdout
+    $stdout = StringIO.new
+    yield
+    $stdout.string
+  ensure
+    $stdout = original_stdout
   end
 
 end
