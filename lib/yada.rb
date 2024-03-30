9d33f3ef9f4997e038dae84b87e393f74c250165
ferbass <ferbass@gmail.com>
2024-03-30 21:17:45 +0900

    big refactor in the self evaluation expression to make the code more readble

--- lib/yada.rb
+++ lib/yada.rb
@@ -60,6 +60,7 @@ class Yada
   def eval(exp, env = @global)
 
     # Self-evaluating expressions
+    #
 
     # Numbers
     if is_number(exp)
@@ -71,104 +72,22 @@ class Yada
       return exp.slice(1, exp.length - 2)
     end
 
-    # If expression
-    # ['if', condition, true, false]
-
-    if exp[0] == 'if'
-      _, condition, true_exp, false_exp = exp
-      return eval(condition, env) ? eval(true_exp, env) : eval(false_exp, env)
-    end
-
-    if exp[0] == 'default'
-      _, default_exp = exp
-      return eval(default_exp, env)
-    end
-
-    # Switch expression
-    # ['switch', ['case1', 'block1'], ['case2', 'block2'], ['default', 'block3']]
-    #
-    if exp[0] == 'switch'
-      if_exp = @transformer.switch_to_if(exp)
-      return eval(if_exp, env)
-    end
-
-    # While expression
-    # ['while', condition, body]
-    # (while (< i 10) (say i))
-
-    if exp[0] == 'while'
-      _, condition, body = exp
-
-      result = nil
-      while eval(condition, env)
-        result = eval(body, env)
-      end
-      return result
-    end
-
-    # For expression
-    # ['for', init, condition, update, body]
-    # (for (var i 0) (< i 10) (set i (+ i 1)) (say i))
-    #
-    # Syntax sugar for: (begin init (while condition (begin body update)))
-    if exp[0] == 'for'
-      while_exp = @transformer.for_to_while(exp)
-      return eval(while_exp, env)
-    end
-
-    # Variables
-
-    # Variable declaration
-    if exp[0] == 'var'
-      _, variable, value = exp
-      return env.define(variable, eval(value, env))
+    case exp[0]
+    when 'begin'   then return eval_begin(exp, env)
+    when 'if'      then return eval_if(exp, env)
+    when 'switch'  then return eval_switch(exp, env)
+    when 'default' then return eval_default(exp, env)
+    when 'while'   then return eval_while(exp, env)
+    when 'for'     then return eval_for(exp, env)
+    when 'var'     then return eval_var(exp, env)
+    when 'set'     then return eval_set(exp, env)
+    when 'defun'   then return eval_defun(exp, env)
+    when 'lambda'  then return eval_lambda(exp, env)
     end
 
     # Variable access
     if is_variable_name(exp)
       return env.lookup(exp)
-    end
-
-    # Set variable
-    # ['set', 'variable', value]
-    # ['set', 'x', 5]
-    if exp[0] == 'set'
-      _, variable, value = exp
-      return env.assign(variable, eval(value, env))
-    end
-
-    # block: sequence of expressions
-    if exp[0] == 'begin'
-      block_env = Environment.new({}, env)
-      return eval_block(exp, block_env)
-    end
-
-    # Function definition
-    # ['defun', 'function_name', ['arg1', 'arg2', ...], body]
-    # (defun add (x y) (+ x y))
-    #
-    # Syntax sugar for: (var add (lambda (x y) (+ x y))
-    if exp[0] == 'defun'
-      _tag, name, args, body = exp
-
-      # JIT-transpile to a variable declaration
-      var_exp = @transformer.defun_to_var_lambda(exp)
-
-      return eval(var_exp, env)
-    end
-
-    # Lambda definition
-    # ['lambda', ['arg1', 'arg2', ...], body]
-    # (lambda (x) (+ x 1))
-
-    if exp[0] == 'lambda'
-      _tag, args, body = exp
-
-      return {
-        args: args,
-        body: body,
-        env: env
-      }
     end
 
     # Function calls
@@ -207,6 +126,103 @@ class Yada
   end
 
   private
+
+  # block: sequence of expressions
+  # ['begin', exp1, exp2, ...]
+  def eval_begin(exp, env)
+    block_env = Environment.new({}, env)
+    return eval_block(exp, block_env)
+  end
+
+  # If expression
+  # ['if', condition, true, false]
+  def eval_if(exp, env)
+    _, condition, true_exp, false_exp = exp
+    return eval(condition, env) ? eval(true_exp, env) : eval(false_exp, env)
+  end
+
+  # Switch expression
+  # ['switch', ['case1', 'block1'], ['case2', 'block2'], ['default', 'block3']]
+  #
+  def eval_switch(exp, env)
+    if_exp = @transformer.switch_to_if(exp)
+    return eval(if_exp, env)
+  end
+
+
+  # While expression
+  # ['while', condition, body]
+  # (while (< i 10) (say i))
+  def eval_while(exp, env)
+    _, condition, body = exp
+    result = nil
+    while eval(condition, env)
+      result = eval(body, env)
+    end
+    return result
+  end
+
+  # Default expression
+  # ['default', default_exp]
+  # (default 10)
+  #
+  def eval_default(exp, env)
+    _, default_exp = exp
+    return eval(default_exp, env)
+  end
+
+  # Function definition
+  # ['defun', 'function_name', ['arg1', 'arg2', ...], body]
+  # (defun add (x y) (+ x y))
+  #
+  # Syntax sugar for: (var add (lambda (x y) (+ x y))
+  def eval_defun(exp, env)
+    _tag, name, args, body = exp
+
+    # JIT-transpile to a variable declaration
+    var_exp = @transformer.defun_to_var_lambda(exp)
+
+    return eval(var_exp, env)
+  end
+
+  # Lambda definition
+  # ['lambda', ['arg1', 'arg2', ...], body]
+  # (lambda (x) (+ x 1))
+  def eval_lambda(exp, env)
+    _tag, args, body = exp
+
+    return {
+      args: args,
+      body: body,
+      env: env
+    }
+  end
+
+  # For expression
+  # ['for', init, condition, update, body]
+  # (for (var i 0) (< i 10) (set i (+ i 1)) (say i))
+  #
+  # Syntax sugar for: (begin init (while condition (begin body update)))
+  def eval_for(exp, env)
+    while_exp = @transformer.for_to_while(exp)
+    return eval(while_exp, env)
+  end
+
+  # Variables
+  # Variable declaration
+  # ['var', 'variable', value]
+  def eval_var(exp, env)
+    _, variable, value = exp
+    return env.define(variable, eval(value, env))
+  end
+
+  # Set variable
+  # ['set', 'variable', value]
+  # ['set', 'x', 5]
+  def eval_set(exp, env)
+    _, variable, value = exp
+    return env.assign(variable, eval(value, env))
+  end
 
   def eval_body(body, env)
     if (body[0] == 'begin')
