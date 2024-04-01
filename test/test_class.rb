2e845b5f4991be08051f382a10f0128cbb691d5a
ferbass <ferbass@gmail.com>
2024-04-01 23:18:31 +0900

    add test for class inheritance

--- test/test_class.rb
+++ test/test_class.rb
@@ -19,6 +19,22 @@ class TestClass < TestYada
     (var p (new Point 10 20))
 
     ((prop p calc) p)', 30)
+
+    # This test should run in the same context as the previous one
+    yada_assert_equal(@yada, '(class Point3D Point
+        (begin
+
+          (defun init (self x y z)
+            (begin
+              ((prop (super Point3D) init) self x y)
+              (set (prop self z) z)))
+
+          (defun calc (self)
+            (+ ((prop (super Point3D) calc) self)
+               (prop self z)))))
+
+      (var p (new Point3D 10 20 30))
+      ((prop p calc) p)', 60)
   end
 
 end
