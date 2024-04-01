1ddc05f7858c8c2a808c7dfbbc740db8c28829e6
ferbass <ferbass@gmail.com>
2024-04-01 23:18:53 +0900

    remove .rb from require

--- test/test_yada.rb
+++ test/test_yada.rb
@@ -1,8 +1,8 @@
 require 'test/unit'
-require './lib/yada.rb'
-require './lib/environment.rb'
-require './parser/yada_parser.rb'
-require_relative 'test_util_yada.rb'
+require './lib/yada'
+require './lib/environment'
+require './parser/yada_parser'
+require_relative 'test_util_yada'
 
 class TestYada < TestUtil
 
@@ -12,16 +12,16 @@ class TestYada < TestUtil
 
 end
 
-require_relative 'test_math_operators.rb'
-require_relative 'test_variables.rb'
-require_relative 'test_self_eval.rb'
-require_relative 'test_block.rb'
-require_relative 'test_if_expression.rb'
-require_relative 'test_while.rb'
-require_relative 'test_built_in_functions.rb'
-require_relative 'test_user_defined_functions.rb'
-require_relative 'test_lambda_functions.rb'
-require_relative 'test_context_stack.rb'
-require_relative 'test_switch_expression.rb'
-require_relative 'test_for_loop.rb'
-require_relative 'test_class.rb'
+require_relative 'test_math_operators'
+require_relative 'test_variables'
+require_relative 'test_self_eval'
+require_relative 'test_block'
+require_relative 'test_if_expression'
+require_relative 'test_while'
+require_relative 'test_built_in_functions'
+require_relative 'test_user_defined_functions'
+require_relative 'test_lambda_functions'
+require_relative 'test_context_stack'
+require_relative 'test_switch_expression'
+require_relative 'test_for_loop'
+require_relative 'test_class'
